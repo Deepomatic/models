433610f7076b6330646facbe19348df125d5c021
Vincent Delaitre <vincent@deepomatic.com>
2023-03-06 15:20:38 +0100

    Bump Tensorflow to v2.9 and use Python 3.10 (#28)

--- Makefile
+++ Makefile
@@ -1,5 +1,14 @@
 ROOT_DIR:=$(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
 
-all:
+all: build-base
+	docker run -v ${ROOT_DIR}:/app -w /app -ti tf-models bash
+
+# Bump requirements
+bump: build-base
+	docker run -v ${ROOT_DIR}:/app -w /app -ti tf-models pip-compile -v requirements.in
+
+build-base:
 	docker build -t tf-models --target base .
-	docker run -v ${ROOT_DIR}/research:/app -w /app -ti tf-models bash
+
+build:
+	docker build -t tf-models .
---
+++ research/setup.py
@@ -0,0 +1,17 @@
+"""Setup script for object_detection."""
+import os
+from pkg_resources import parse_requirements
+from setuptools import find_packages
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), '..', 'requirements.in'), 'r') as f:
+    requirements = list(map(str, parse_requirements(f)))
+
+setup(
+    name='object_detection',
+    version='2.9',
+    install_requires=requirements,
+    include_package_data=True,
+    packages=[p for p in find_packages() if p.startswith('object_detection')],
+    description='Tensorflow Object Detection Library',
+)
